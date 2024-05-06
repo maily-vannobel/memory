73af977b30fb35792657a045f5797726bef0f8cf
maily-vannobel <maily.vannobel@laplateforme.io>
2024-05-06 10:55:34 +0200

    ajout de cartes, changement images + fonction flip cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,13 +3,18 @@ import './css/card.css'
 
 
 
-function GameCard({ image }) {
+function GameCard({ card, onClick }) {
   return (
-    <div className='carte'>
-      <img src={image} />
+    <div className='card' onClick={() => onClick(card.id)}>
+      <div className={`card-inner ${card.isFlipped ? 'is-flipped' : ''}`}>
+        <div className="card-front">
+          <img src={card.image} alt={`Card ${card.id}`} />
+        </div>
+        <div className="card-back">
+          {/* Mettez ici l'image ou la couleur du dos de la carte */}
+        </div>
+      </div>
     </div>
-    
-
   );
 }
 
--- src/components/gameboard.jsx
+++ src/components/gameboard.jsx
@@ -1,40 +1,57 @@
 import React, { useState } from 'react';
 import MyButton from './button';
+import GameCard from './card'
+import './css/gameBoard.css'
+
 
 function GameBoard() {
+
   const cardsData = [
-    { id: 1, image: './src/assets/images/flower.jpg' },
-    { id: 2, image: './src/assets/images/butterfly.jpg' },
-    { id: 3, image: './src/assets/images/cloud.png' },
-    { id: 4, image: './src/assets/images/flower.jpg' },
-    { id: 5, image: './src/assets/images/butterfly.jpg' },
-    { id: 6, image: './src/assets/images/cloud.png' }
+    { id: 1, image: './src/assets/images/flower.jpg', isFlipped: false },
+    { id: 2, image: './src/assets/images/butterfly.png',isFlipped: false },
+    { id: 3, image: './src/assets/images/cloud.png',isFlipped: false },
+    { id: 4, image: './src/assets/images/sun.png',isFlipped: false},
+    { id: 5, image: './src/assets/images/mushroom.png',isFlipped: false },
+    { id: 6, image: './src/assets/images/princess.png',isFlipped: false },
+    { id: 7, image: './src/assets/images/flower.jpg',isFlipped: false },
+    { id: 8, image: './src/assets/images/butterfly.png',isFlipped: false },
+    { id: 9, image: './src/assets/images/cloud.png',isFlipped: false },
+    { id: 10, image: './src/assets/images/sun.png',isFlipped: false },
+    { id: 11, image: './src/assets/images/mushroom.png',isFlipped: false },
+    { id: 12, image: './src/assets/images/princess.png',isFlipped: false },
+
   ];
+// useState ici pour déclarer état local des cartes + initialise l'état
+ const [cards, setCards] = useState(() => shuffle([...cardsData]));
 
-  const [shuffledCards, setShuffledCards] = useState(() => shuffle([...cardsData]));
-
+ //fonction pour mélanger cartes
   function shuffle(array) {
-    let currentIndex = array.length,
-      randomIndex;
-
+    let currentIndex = array.length, randomIndex;
     while (currentIndex !== 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
       [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
     }
-
     return array;
   }
 
+   //fonction pour gérer clics cartes; mets à jour l'état d'une carte qd elle est cliquée
+  function handleCardClick(id) {
+    const newCards = cards.map(card =>
+      card.id === id ? {...card, isFlipped: !card.isFlipped} : card
+    );
+    setCards(newCards);
+  }
+//gérer clic sur bouton mélange, utilise 'shuffle' pour mélanger +maj
   function handleShuffle() {
-    setShuffledCards(shuffle([...cardsData]));
+    setCards(shuffle([...cardsData]));
   }
 
   const divStyle = {
     display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    gridTemplateRows: 'repeat(2, 1fr)',
-    gap: '20px'
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    gridTemplateRows: 'repeat(4, 1fr)',
+    gap: '10px'
   };
 
   const carteStyle = {
@@ -43,11 +60,20 @@ function GameBoard() {
 
   return (
     <div style={divStyle}>
-      {shuffledCards.map((card, index) => (
-        <img style={carteStyle} key={index} src={card.image} alt={`Card ${card.id}`} />
+<section>
+      
+          <MyButton text="Mélanger" onClick={handleShuffle} />
+          <MyButton text="Lancer une partie"  /> 
+     
+</section>
+ <section class="board">
+      {cards.map((card, index) => (
+        <GameCard key={index} card={card} onClick={handleCardClick} style={carteStyle} />
       ))}
-      <MyButton text="Mélanger" onClick={handleShuffle} />
+
+</section>
     </div>
+
   );
 }
 
